4dc06b7d84355abb91c442a5d704bb9be18f8581
SuperLonci <55552282+SuperLonci@users.noreply.github.com>
2025-03-16 23:24:53 +0100

    Add welcome page

---
+++ immiq/src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+</script>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start; /* Align items to the top */
+        height: 100vh;
+        text-align: center;
+        padding-top: 15vh; /* Add some padding from the top */
+    }
+
+    .welcome {
+        font-size: 2rem;
+        margin-bottom: 1rem;
+    }
+
+    .get-started {
+        font-size: 1.2rem;
+        color: #555;
+    }
+</style>
+
+<div class="container">
+    <h1 class="welcome">Welcome to ImmiQ</h1>
+    <p class="get-started">Get started by selecting an entry on the sidebar.</p>
+</div>

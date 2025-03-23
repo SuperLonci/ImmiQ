5ac17897299232c7584a25733524d8a5352252a7
SuperLonci <55552282+SuperLonci@users.noreply.github.com>
2025-03-23 13:41:46 +0100

    Mark ToDo to evaluate svg not showing up

--- src/components/EntityList.svelte
+++ src/components/EntityList.svelte
@@ -35,6 +35,7 @@
             {#if showAddButton}
                 <button on:click={handleAddNew} class="add-button" aria-label="Add new">
                     +
+                    <!--                 ToDo: evaluate why the svg doesnt get shown-->
                     <!--                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"-->
                     <!--                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">-->
                     <!--                        <line x1="12" y1="5" x2="12" y2="19"></line>-->
